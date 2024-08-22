a2d4ccb1520b08600fe3de3586acde938f271d50
jihkang <106561135+jihkang@users.noreply.github.com>
2024-08-22 18:09:39 +0900

    video toggle

--- src/components/camera.tsx
+++ src/components/camera.tsx
@@ -1,10 +1,31 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
+
+const getVideoStream = async (
+  videoRef: MutableRefObject<HTMLVideoElement | null>
+) => {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+  if (videoRef.current) {
+    videoRef.current.srcObject = stream;
+  }
+};
+
+const videoStreamCleanup = (
+  videoRef: MutableRefObject<HTMLVideoElement | null>
+) => {
+  if (videoRef.current && videoRef.current.srcObject) {
+    const stream = videoRef.current.srcObject as MediaStream;
+    const tracks = stream.getTracks();
+    tracks.forEach((track) => track.stop());
+  }
+};
 
 export default function Cam() {
   const [hydrate, setHydrate] = useState(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  const videoOffRecordRef = useRef<boolean>(false);
   useEffect(() => {
     if (hydrate === false) {
       setHydrate(true);
@@ -12,21 +33,9 @@ export default function Cam() {
   }, [hydrate]);
 
   useEffect(() => {
-    const getVideoStream = async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-
-      if (videoRef.current) {
-        videoRef.current.srcObject = stream;
-      }
-    };
-    getVideoStream();
-
+    getVideoStream(videoRef);
     return () => {
-      if (videoRef.current && videoRef.current.srcObject) {
-        const stream = videoRef.current.srcObject as MediaStream;
-        const tracks = stream.getTracks();
-        tracks.forEach((track) => track.stop());
-      }
+      videoStreamCleanup(videoRef);
     };
   }, []);
 
@@ -35,7 +44,20 @@ export default function Cam() {
   }
   return (
     <div>
-      <video ref={videoRef} autoPlay muted />
+      <video
+        ref={videoRef}
+        autoPlay
+        muted
+        onDoubleClick={() => {
+          if (videoOffRecordRef.current === false) {
+            videoStreamCleanup(videoRef);
+            videoOffRecordRef.current = true;
+          } else {
+            getVideoStream(videoRef);
+            videoOffRecordRef.current = false;
+          }
+        }}
+      />
     </div>
   );
 }
